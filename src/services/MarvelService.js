017d2918e727a7b5687c6e72815b6108691e831e
Yevhenii <organistiy@gmail.com>
2022-12-01 13:17:53 +0100

    create new project

---
+++ src/services/MarvelService.js
@@ -0,0 +1,26 @@
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=9676fdeddb29176a71499bb800c89c37'
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    }
+
+    getCharacter = (id) => {
+        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    }
+}
+
+
+
+export default MarvelService;
