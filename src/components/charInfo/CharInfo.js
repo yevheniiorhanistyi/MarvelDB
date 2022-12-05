1317bd6ad1837cc650e2cee6dcfd227355dcc70e
Yevhenii <organistiy@gmail.com>
2022-12-05 19:51:42 +0100

    was added check prop type 'charId'

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
@@ -128,5 +129,9 @@ const View = ({ char }) => {
         </>
     )
 }
+
+// CharInfo.protoTypes = {
+//     charId: PropTypes.string
+// }
 
 export default CharInfo;
