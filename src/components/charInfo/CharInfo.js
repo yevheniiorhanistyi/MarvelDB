9a125adc73627177babda180c0abc70a2cb58d7b
Yevhenii <organistiy@gmail.com>
2022-12-12 22:56:52 +0100

    fix

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
