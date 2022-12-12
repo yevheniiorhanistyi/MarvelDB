35666af1997c33d97bdd1b829fdc3a413d4ed20d
Yevhenii <organistiy@gmail.com>
2022-12-12 12:53:14 +0100

    revert all files

---
+++ src/services/MarvelService.js
@@ -0,0 +1,46 @@
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=9676fdeddb29176a71499bb800c89c37';
+    _baseOffset = 211;
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        const altDescription = 'There should be a short description of the character here, but unfortunately not yet...';
+
+        return {
+            id: char.id,
+            name: char.name,
+            description: char.description ?
+                char.description.length > 230 ?
+                    char.description.slice(0, 210) + '...' :
+                    char.description : altDescription,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+            comics: char.comics.items
+        }
+    }
+}
+
+
+
+export default MarvelService;
