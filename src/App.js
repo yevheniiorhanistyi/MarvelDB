84911d91696f7164a84441297a8705a4cf122f3c
Yevhenii <organistiy@gmail.com>
2023-01-26 14:07:26 +0100

    optimazed pages - lazy/Suspens

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import AppHeader from "./components/appHeader/AppHeader";
-import { Main, Comics, Page404, SingleComic } from './pages';
+import Spinner from "./components/spinner/Spinner";
+
+const Main = lazy(() => import(/* webpackChunkName: "Main" */'./pages/Main'));
+const Comics = lazy(() => import(/* webpackChunkName: "Comics" */'./pages/Comics'));
+const SingleComic = lazy(() => import(/* webpackChunkName: "SingleComic" */'./pages/SingleComic'));
+const Page404 = lazy(() => import(/* webpackChunkName: "Page404" */'./pages/404'));
 
 const App = () => {
 
@@ -11,10 +17,26 @@ const App = () => {
                 <AppHeader />
                 <main>
                     <Routes>
-                        <Route path="/" element={<Main />} />
-                        <Route path="/comics" element={<Comics />} />
-                        <Route path="comics/:id" element={<SingleComic />} />
-                        <Route path="*" element={<Page404 />} />
+                        <Route path="/" element={
+                            <Suspense fallback={<Spinner />}>
+                                <Main />
+                            </Suspense>
+                        } />
+                        <Route path="/comics" element={
+                            <Suspense fallback={<Spinner />}>
+                                <Comics />
+                            </Suspense>
+                        } />
+                        <Route path="/comics/:id" element={
+                            <Suspense fallback={<Spinner />}>
+                                <SingleComic />
+                            </Suspense>
+                        } />
+                        <Route path="*" element={
+                            <Suspense fallback={<Spinner />}>
+                                <Page404 />
+                            </Suspense>
+                        } />
                     </Routes>
                 </main>
             </div>
